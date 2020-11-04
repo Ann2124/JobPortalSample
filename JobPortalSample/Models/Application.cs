9d5e97f38276b64132ed3bae7835d229c6e18456
G3-K <gayu.kunnath@gmail.com>
2020-11-04 11:05:59 +0530

    applictn class

--- JobPortalSample/Models/Application.cs
+++ JobPortalSample/Models/Application.cs
@@ -7,5 +7,19 @@ namespace JobPortalSample.Models
 {
     public class Application
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int applicationId { get; set; }
+        [Required]
+        [Display(Name = "Email Id")]
+        public string email { get; set; }
+        [Required]
+        [Display(Name = "Job Id")]
+        public int jobId { get; set; }
+        public string status { get; set; }
+
+
+        public virtual User user { get; set; }
+        public virtual Openings Openings { get; set; }
     }
 }
