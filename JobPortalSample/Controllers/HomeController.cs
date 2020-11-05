4d371fc95a53da7b482581907d016d263c6cc966
Ann Liya <annliya4@gmail.com>
2020-11-05 14:22:16 +0530

    .

--- JobPortalSample/Controllers/HomeController.cs
+++ JobPortalSample/Controllers/HomeController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using JobPortalSample.Models;
+using System.IO;
+using System.Data.Entity;
+using System.Web.Security;
 
 namespace JobPortalSample.Controllers
 {
     public class HomeController : Controller
     {
+        
         public ActionResult Index()
         {
             return View();
