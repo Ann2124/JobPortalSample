d42539b81b6e6bb62b41f426e7b65c47daa8294e
Ann Liya <annliya4@gmail.com>
2020-11-06 14:59:23 +0530

    .

---
+++ JobPortalSample/Models/JobPortalContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace JobPortalSample.Models
+{
+    public class JobPortalContext:DbContext
+    {
+        public JobPortalContext():base("Name=Dbconfig")
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Application> Applications { get; set; }
+        public DbSet<Openings> Openings { get; set; }
+        public DbSet<Employer> Employers { get; set; }
+    }
+}
